26ae1ba79d3874a3e612b183070621ce20c1df04
Yanisa <yanisa.o@rmutsvmail.com>
2020-07-14 18:00:44 +0700

    add code 2lines

--- src/Lab1/HelloWorld.java
+++ src/Lab1/HelloWorld.java
@@ -5,6 +5,10 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
         System.out.println("Yanisa Oiadmuang");
+        System.out.println("Film");
+        System.out.println("0612039621");
+
+
 
     }
 }
