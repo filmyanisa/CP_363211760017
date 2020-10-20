5914c71738039fb9b7c55dd98add1d70d946f9b8
yanisa <yanisa.o@rmutsvmail.com>
2020-10-20 16:12:54 +0700

    Lab 2 : basic input

--- src/Exersice/Exercise_Lab4_1.java
+++ src/Exersice/Exercise_Lab4_1.java
@@ -1,67 +1,69 @@
 package Exersice;
-//1.Triangle = 0.5* b *h
-//2.Cone = (Pi * r * l) + (Pi * r * r)
-//3.Circle = 2 * Pi * r
-
-
 import java.util.Scanner;
-
+//1.Triangle = 0.5* b * h
+//2.Cone     = (Pi * r * l) + (Pi * r * r)
+//3.Circle = 2 * Pi * r
 public class Exercise_Lab4_1 {
-    public static  final  double PI = 3.141;
+
+    public  static  final  double PI = 3.14;
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner SC = new Scanner(System.in);
+
+
         //display menu
-        System.out.println("Please,selsct options below:  ");
-        System.out.println("1.Find area of Triangle");
-        System.out.println("2.Find area of Cone");
-        System.out.println("3.Find area of Circle");
-        System.out.println("4.Exiit");
+        System.out.println("Please, select options below: ");
+        System.out.println("1. Find area of Triangle");
+        System.out.println("2. Find area of Cone");
+        System.out.println("3. Find area of Circle");
+        System.out.println("4. Exit: ");
         System.out.println("Select: ");
-        int select = sc.nextInt();
-
-
-
+        int select = SC.nextInt();
         //switch-case
-        switch(select){
-            case 1:
-                System.out.print("Enter base:  ");
-                double b = sc.nextDouble();
-                System.out.print("Enter high:  ");
-                double h= sc.nextDouble();
-                double area = findArea(b,h);
-                System.out.println("The area of Triangle:"+area);
+        switch (select){
+            case  1:
+                System.out.println("Enter base: ");
+                double b = SC.nextDouble();
+                System.out.println("Enter high: ");
+                double h = SC.nextDouble();
+                double area = findArea(b, h);
+                System.out.println("The area of Triangle : "+area);
                 break;
-            case 2:
-                System.out.print("Enter รัศมีของทรงกรวย:  ");
-                double r = sc.nextDouble();
-                System.out.print("Enter ความสูงเอียงของทรงกรวย :  ");
-                double l = sc.nextDouble();
-                double cone = findAreaCone(r,l);
-                System.out.println("The area of Cone:"+cone);
+            case  2:
+                System.out.println("Enter รัศมีของทรงกรวย: ");
+                double r = SC.nextDouble();
+                System.out.println("Enter ความสูงเอียงของทรงกรวย: ");
+                double l = SC.nextDouble();
+                double cone = findAreaCone(r, l);
+                System.out.println("The area of Cone: "+cone);
                 break;
-            case 3:
-                System.out.print("Enter :รัศมีของวงกลม ");
-                double PI = sc.nextDouble();
+            case  3:
+                System.out.println("Enter : รัศมีของวงกลม ");
+                double PI = SC.nextDouble();
                 double Circle = findAreaCircle(PI);
-                System.out.println("The area of Circle:"+Circle);
+                System.out.println("The area of Circle: "+Circle);
                 break;
-            case 4: System.exit(1);break;
-            default:System.out.println("Please, select number 1-4 ");
+            case  4: System.exit(1);  break;
+            default: System.out.println("Please, select number 1-4 ");
+
+
         }
+
 
 
 
     }
     //triangle
-    public static double findArea(double b, double h){
-        return  0.5 * b * h;
+    public static double findArea(double b, double h) {
+        return 0.5 * b * h;
+
+
     }
     //Cone
-    public static double findAreaCone(double r,double l) {
-        return ( PI * r * l ) + ( PI * r * r);
+    public static double findAreaCone(double r, double l) {
+        return(PI * r *l) + (PI * r * r);
     }
     //Circle
     public static double findAreaCircle(double r) {
-        return 2 * PI * r;
+        return 2 * PI *r;
     }
-}
+}//class
---
+++ src/Exersice/Exersice_Lab8_1.java
@@ -0,0 +1,21 @@
+package Exersice;
+
+public class Exersice_Lab8_1 {
+
+    public static void mai (String[] args){
+        int [] number = {10,20,30,40,50};
+        int max = FindMax(number);
+        System.out.println("The maximum number is "+max);
+
+    }
+
+    private static int FindMax(int[] number) {
+        int max = number[0];
+        for (int i = 0; i<= 5; i++) {
+            if (max <i = number [i])
+                max = number [i];
+
+        }
+        return max;
+    }
+}
