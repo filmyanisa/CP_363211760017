f00ad315f1b060fb9d32d8e0cf96fea8d7f81664
yanisa <yanisa.o@rmutsvmail.com>
2020-10-27 14:29:31 +0700

    Lab 2 : basic input

--- src/Lab8/ExThrows.java
+++ src/Lab8/ExThrows.java
@@ -2,28 +2,22 @@ package Lab8;
 
 public class ExThrows {
 
-
-    public static void main(String args) {
+    public static void main(String[] args) {
         int x = 10;
         int y = 0;
 
         int r = 0;
         try {
             r = divide(x, y);
-        } catch (Exception e ){
+        } catch (Exception e) {
             e.printStackTrace();
-
         }
-
-
-        System.out.println("The result :" + r);
+        System.out.println("The result : "+r);
 
 
     }
 
-    private static int divide(int x, int y) throws Exception {
-        return x / y;
-
+    private static int divide(int x, int y)throws Exception {
+        return x / y;   // 10/0
     }
 }
-
